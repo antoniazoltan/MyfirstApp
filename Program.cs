40f7a8d4b266ff5ddc8befc2fe9744e907881c11
Antonia Zoltan <antonia.zoltan@ebs.msft>
2021-02-17 19:39:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyfirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
